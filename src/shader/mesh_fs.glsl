e8c620968a08b997c9c5a682bacb2c9edc90c1b8
Diharaw <shredhead94@gmail.com>
2019-11-17 21:41:30 +0000

    CSM integration.

--- src/shader/mesh_fs.glsl
+++ src/shader/mesh_fs.glsl
@@ -8,17 +8,27 @@ out vec3 FS_OUT_Color;
 // INPUT VARIABLES  -------------------------------------------------
 // ------------------------------------------------------------------
 
-in vec3 FS_IN_ObjPos;
 in vec3 FS_IN_WorldPos;
 in vec3 FS_IN_Normal;
 in vec2 FS_IN_UV;
 in vec2 FS_IN_LightmapUV;
+in vec4 FS_IN_NDCFragPos;
 
 // ------------------------------------------------------------------
 // UNIFORMS ---------------------------------------------------------
 // ------------------------------------------------------------------
 
+layout(std140, binding = 1) buffer CSMUniforms
+{
+	mat4 texture_matrices[8];
+    vec4 direction;
+    vec4 options;
+    int num_cascades;
+    float far_bounds[8];
+};
+
 uniform sampler2D s_Lightmap;
+uniform sampler2DArray s_ShadowMap;
 
 // ------------------------------------------------------------------
 // FUNCTIONS  -------------------------------------------------------
@@ -46,13 +56,63 @@ vec3 exposed_color(vec3 color)
 }
 
 // ------------------------------------------------------------------
+
+float shadow_occlussion(float frag_depth, vec3 n, vec3 l)
+{
+	int index = 0;
+    float blend = 0.0;
+    
+	// Find shadow cascade.
+	for (int i = 0; i < num_cascades - 1; i++)
+	{
+		if (frag_depth > far_bounds[i])
+			index = i + 1;
+	}
+
+	blend = clamp( (frag_depth - far_bounds[index] * 0.995) * 200.0, 0.0, 1.0);
+    
+    // Apply blend options.
+    blend *= options.z;
+
+	// Transform frag position into Light-space.
+	vec4 light_space_pos = texture_matrices[index] * vec4(FS_IN_WorldPos, 1.0f);
+
+	float current_depth = light_space_pos.z;
+    
+	float bias = max(0.0005 * (1.0 - dot(n, l)), 0.0005);  
+
+	float shadow = 0.0;
+	vec2 texelSize = 1.0 / textureSize(s_ShadowMap, 0).xy;
+
+	for(int x = -1; x <= 1; ++x)
+	{
+	    for(int y = -1; y <= 1; ++y)
+	    {
+	        float pcfDepth = texture(s_ShadowMap, vec3(light_space_pos.xy + vec2(x, y) * texelSize, float(index))).r; 
+	        shadow += current_depth - bias > pcfDepth ? 1.0 : 0.0;        
+	    }    
+	}
+
+	shadow /= 9.0
+
+    return shadow;    
+}
+
+// ------------------------------------------------------------------
 // MAIN -------------------------------------------------------------
 // ------------------------------------------------------------------
 
 void main()
 {
+    vec3 n = normalize(FS_IN_Normal);
+	vec3 l = -direction.xyz;
+
+	float frag_depth = (FS_IN_NDCFragPos.z / FS_IN_NDCFragPos.w) * 0.5 + 0.5;
+	float shadow = shadow_occlussion(frag_depth, n, l);
+
     vec3 color   = exposed_color(texture(s_Lightmap, FS_IN_LightmapUV).rgb);
-    FS_OUT_Color = linear_to_srgb(color);
+
+    FS_OUT_Color = shadow * vec3(1.0) + linear_to_srgb(color);
 }
 
 // ------------------------------------------------------------------
--- src/shader/shadow_map_vs.glsl
+++ src/shader/shadow_map_vs.glsl
@@ -1,5 +1,5 @@
 // ------------------------------------------------------------------
-// INPUT VARIABLES --------------------------------------------------
+// INPUTS -----------------------------------------------------------
 // ------------------------------------------------------------------
 
 layout(location = 0) in vec3 VS_IN_Position;
@@ -10,27 +10,19 @@ layout(location = 4) in vec3 VS_IN_Tangent;
 layout(location = 5) in vec3 VS_IN_Bitangent;
 
 // ------------------------------------------------------------------
-// OUTPUT VARIABLES -------------------------------------------------
-// ------------------------------------------------------------------
-
-out vec3 FS_IN_WorldPos;
-out vec3 FS_IN_Normal;
-out vec2 FS_IN_UV;
-out vec2 FS_IN_LightmapUV;
-out vec4 FS_IN_NDCFragPos;
-
-// ------------------------------------------------------------------
 // UNIFORMS ---------------------------------------------------------
 // ------------------------------------------------------------------
 
-layout(std140) uniform GlobalUniforms
+layout(std430, binding = 0) buffer GlobalUniforms
 {
     mat4 view_proj;
     mat4 light_view_proj;
     vec4 cam_pos;
+    mat4 crop[8];
 };
 
 uniform mat4 u_Model;
+uniform int u_CascadeIndex;
 
 // ------------------------------------------------------------------
 // MAIN -------------------------------------------------------------
@@ -38,14 +30,7 @@ uniform mat4 u_Model;
 
 void main()
 {
-    vec4 world_pos   = u_Model * vec4(VS_IN_Position, 1.0f);
-    FS_IN_WorldPos   = world_pos.xyz;
-    FS_IN_Normal     = normalize(normalize(mat3(u_Model) * VS_IN_Normal));
-    FS_IN_UV         = VS_IN_UV;
-    FS_IN_LightmapUV = VS_IN_LightmapUV;
-    FS_IN_NDCFragPos = view_proj * world_pos;
-
-    gl_Position = FS_IN_NDCFragPos;
+    gl_Position = crop[u_CascadeIndex] * u_Model * vec4(VS_IN_Position, 1.0);
 }
 
 // ------------------------------------------------------------------
